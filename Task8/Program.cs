a0668b387e038716d42fcd2841f18167dec41a38
Dmitry Molchanov <admin@nevinka-school1.ru>
2022-08-19 09:59:25 +0300

    Added solution to task #8

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+
+Console.Write("Enter a number N greater than 1: ");
+int number = Convert.ToInt32(Console.ReadLine()),
+    count=1;
+if (number > 0) 
+{
+    Console.Write($"All even numbers in the range [1; {number}]: ");
+    while (count <= number)
+    {     
+        if (count%2 == 0)    
+        {
+            Console.Write($"{count}");
+            if (count != number/2*2)    
+            Console.Write(", ");         // Output comma until end of output is reached [for beauty ;) ]
+        }
+    
+        count++;
+    }
+}
+else
+Console.Write($"Please enter a number greater than 1 next time.");
