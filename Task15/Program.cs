60828661ac593f02b90016340cc392f5084bae05
Dmitry Molchanov <admin@nevinka-school1.ru>
2022-08-22 19:18:08 +0300

    Fixed the solution to task #15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,13 +1,19 @@
-﻿using System;
-using System.Globalization;
- 
-class Program
-{
-    static void Main(string[] args)
-    {
-        uint day = uint.Parse(Console.ReadLine());
-        string[] days = CultureInfo.CurrentCulture.DateTimeFormat.DayNames;
-        Console.WriteLine(day == 7 ? days[0] : day <= 6 && day >= 1 ? days[day] : "ERROR");
-        Console.ReadKey(true);
-    }
-}
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+ int InputNumbers()
+ {
+    int numberday; 
+    do Console.WriteLine("Enter the number of the day of the week");
+    while (!Int32.TryParse(Console.ReadLine(), out numberday) || numberday < 1 || numberday > 7);
+    return numberday;
+ }
+        
+void CheckNumber(int num) 
+ {
+    if (num > 5) Console.WriteLine("Yes, this day is a day off");
+    else  Console.WriteLine("No, it's a working day");
+ }
+
+Console.Clear();
+int inputday = InputNumbers();
+CheckNumber(inputday);
