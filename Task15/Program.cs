121556cfcd69313ea4d231b456c3037a742d9aee
Dmitry Molchanov <admin@nevinka-school1.ru>
2022-08-22 19:15:06 +0300

    Added solution to task #15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+ 
+class Program
+{
+    static void Main(string[] args)
+    {
+        uint day = uint.Parse(Console.ReadLine());
+        string[] days = CultureInfo.CurrentCulture.DateTimeFormat.DayNames;
+        Console.WriteLine(day == 7 ? days[0] : day <= 6 && day >= 1 ? days[day] : "ERROR");
+        Console.ReadKey(true);
+    }
+}
