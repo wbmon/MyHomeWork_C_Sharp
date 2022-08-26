c51e19e26e5174ae81be58be19f6347fad577a52
Dmitry Molchanov <admin@nevinka-school1.ru>
2022-08-26 16:47:46 +0300

    Added the solution to task #19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+int ReverseNumber(int number)
+{
+    int temp = number;
+    int reverse = 0;
+    while(number > 0)
+        {
+            int digit = number % 10;
+            reverse = reverse * 10 + digit;
+            number = number / 10;
+        }
+    return reverse;
+}
+
+void CheckReverse(int checknum, int numb)
+{
+if (checknum == numb) Console.WriteLine("The entered number is a palindrome!");
+else Console.WriteLine("The entered number is not a palindrome!");
+}
+
+Console.Clear();
+Console.Write("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int rebnum = ReverseNumber(number);
+CheckReverse(rebnum, number);
