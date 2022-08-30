e9acc0f1a2d6ad8d146e82b4426c8c70e0970c48
Dmitry Molchanov <admin@nevinka-school1.ru>
2022-08-30 20:37:03 +0300

    Added the solution to task #29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int [] CreateArray(int n)
+{
+  int [] group = new int[n];
+  for (int i = 0; i < n; i++)
+  {
+    Console.Write($"Enter the {i+1} element of the array: ");
+    group[i] = Convert.ToInt32(Console.ReadLine());
+  }
+  return group;
+}
+
+void PrintArry(int[] mas)
+{
+  int count = mas.Length;
+  Console.Write("[");
+  for(int index = 0; index < count; index++)
+  {
+    Console.Write(mas[index]);
+    if (index < count - 1) Console.Write(", ");
+  }
+  Console.Write("]");
+} 
+
+int [] arNum = CreateArray(8); // If necessary, i can add the ability to set the dimension of the array.
+PrintArry(arNum);
