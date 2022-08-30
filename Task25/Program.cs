67b17ce8d7b641330ab72f759f356c1d361c63bd
Dmitry Molchanov <admin@nevinka-school1.ru>
2022-08-30 07:28:53 +0300

    Added the solution to task #25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int RaiseToADegree(int numA, int numB)
+{
+  int C = 1;
+  for(int i=1; i <= numB; i++)
+    C = C * numA;
+  return C;
+}
+
+Console.Clear();
+
+Console.Write("Enter a number A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter a number B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int degree = RaiseToADegree(A, B);
+Console.WriteLine("A ^ B = " + degree);
