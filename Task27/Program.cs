995cd3463f46ef2b605e8038cc4473a9e5aca845
Dmitry Molchanov <admin@nevinka-school1.ru>
2022-08-30 07:40:27 +0300

    Added the solution to task #27

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+  int SumDigits(int N)
+  {
+    int counter = Convert.ToString(N).Length;
+    int step = 0;
+    int res = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+      step = N - N % 10;
+      res = res + (N - step);
+      N = N / 10;
+    }
+   
+   return res;
+  }
+
+Console.Clear();
+
+Console.Write("Enter a number N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+int sum = SumDigits(numN);
+Console.WriteLine($"The sum of the digits in the number {numN} is equal to " + sum);
