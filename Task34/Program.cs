06034b3c8c33afbc3d8a19ca9489bfb4fd4550c7
Dmitry Molchanov <admin@nevinka-school1.ru>
2022-09-01 07:19:55 +0300

    Added the solution to task #34

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -1,2 +1,25 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 
+int CreateRandomArray (int dimens, int min, int max)
+{
+    int [] mas = new int[dimens];
+    int sum = 0;
+    Console.WriteLine("Array generated: ");
+    Console.Write("[");
+    for(int i = 0; i < dimens; i++) 
+    {
+        mas[i] = new Random().Next(min, max);
+        if (i < dimens - 1) Console.Write($"{mas[i]}, ");
+            else Console.Write($"{mas[i]}");
+        if (i % 2 != 1) sum += mas[i];
+    }
+    Console.WriteLine("]");
+    return sum;
+}
+
+Console.Clear();
+Console.Write("Enter the dimension of the array: ");
+int numberElements = Convert.ToInt32(Console.ReadLine());
+
+int sumEl = CreateRandomArray(numberElements, 100, 1000);
+Console.WriteLine($"The sum of the elements in odd positions is equal to {sumEl}, ");
