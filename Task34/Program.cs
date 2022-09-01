b508a398587c5cde5612a67bcda7f8a3562c465c
Dmitry Molchanov <admin@nevinka-school1.ru>
2022-09-01 07:34:00 +0300

    Fixed the solution to task #34

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -11,7 +11,7 @@ int CreateRandomArray (int dimens, int min, int max)
         mas[i] = new Random().Next(min, max);
         if (i < dimens - 1) Console.Write($"{mas[i]}, ");
             else Console.Write($"{mas[i]}");
-        if (i % 2 != 1) sum += mas[i];
+        if (mas[i] % 2 == 0) sum ++;
     }
     Console.WriteLine("]");
     return sum;
@@ -21,5 +21,5 @@ Console.Clear();
 Console.Write("Enter the dimension of the array: ");
 int numberElements = Convert.ToInt32(Console.ReadLine());
 
-int sumEl = CreateRandomArray(numberElements, 100, 1000);
-Console.WriteLine($"The sum of the elements in odd positions is equal to {sumEl}, ");
+int numEl = CreateRandomArray(numberElements, 100, 1000);
+Console.WriteLine($"Even elements in the array {numEl}. ");
