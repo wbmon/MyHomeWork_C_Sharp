abe5d33bcd6291748ed3086ea6f04202e5b9e19a
Dmitry Molchanov <admin@nevinka-school1.ru>
2022-09-01 20:10:16 +0300

    Added the solution to task #38

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -1,2 +1,44 @@
 ﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементами массива.
 
+double[] CreateArray(int n, int min, int max)
+{
+    double[] mas = new double[n];
+
+    Console.WriteLine("Array generated: ");
+    Console.Write("[");
+
+    Random rnd = new Random();
+        for (int i = 0; i < mas.GetLength(0); i++)
+        {
+            mas[i] = Math.Round((rnd.NextDouble() * (max - min) + min), 2);
+
+            if (i < n - 1) Console.Write($"{mas[i]}, ");
+            else Console.Write($"{mas[i]}");
+            
+        }
+    Console.WriteLine("]");
+    return mas;
+}
+
+Console.Clear();
+Console.Write("Enter the dimension of the array: ");
+int dimens=Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the LOWER limit of the range of values: ");
+int minlim = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the UPPER limit of the range of values: ");
+int maxlim = Convert.ToInt32(Console.ReadLine());
+
+
+double[] massive = CreateArray(dimens, minlim, maxlim);
+double dif = 0;
+double minval = massive[0];
+double maxval = massive[0];
+    for (int i = 0; i < massive.GetLength(0); i++)
+        {
+            if (minval > massive[i]) minval = massive[i];
+            if (maxval < massive[i]) maxval = massive[i];
+        }
+dif = Math.Round(maxval - minval,2);
+ Console.WriteLine($"The difference between the maximum {maxval} and minimum {minval} elements of the array: {dif}");
