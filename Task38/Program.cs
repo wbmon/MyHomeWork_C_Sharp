40aa9439ee8f3cdc158e11c44bad5049533e0b54
Dmitry Molchanov <admin@nevinka-school1.ru>
2022-09-01 20:55:44 +0300

    Fixed the solution to task #38

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -4,20 +4,23 @@ double[] CreateArray(int n, int min, int max)
 {
     double[] mas = new double[n];
 
+    Random rnd = new Random();
+        for (int i = 0; i < mas.GetLength(0); i++) 
+            mas[i] = Math.Round((rnd.NextDouble() * (max - min) + min), 2);
+    return mas;
+}
+void ShowArray(double [] mas)
+{
     Console.WriteLine("Array generated: ");
     Console.Write("[");
-
-    Random rnd = new Random();
-        for (int i = 0; i < mas.GetLength(0); i++)
+    
+    for (int i = 0; i < mas.Length; i++)
         {
-            mas[i] = Math.Round((rnd.NextDouble() * (max - min) + min), 2);
-
-            if (i < n - 1) Console.Write($"{mas[i]}, ");
+            if (i < mas.Length - 1) Console.Write($"{mas[i]}, ");
             else Console.Write($"{mas[i]}");
-            
         }
     Console.WriteLine("]");
-    return mas;
+
 }
 
 Console.Clear();
@@ -30,8 +33,10 @@ int minlim = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter the UPPER limit of the range of values: ");
 int maxlim = Convert.ToInt32(Console.ReadLine());
 
-
 double[] massive = CreateArray(dimens, minlim, maxlim);
+
+ShowArray(massive);
+
 double dif = 0;
 double minval = massive[0];
 double maxval = massive[0];
@@ -41,4 +46,4 @@ double maxval = massive[0];
             if (maxval < massive[i]) maxval = massive[i];
         }
 dif = Math.Round(maxval - minval,2);
- Console.WriteLine($"The difference between the maximum {maxval} and minimum {minval} elements of the array: {dif}");
+Console.WriteLine($"The difference between the maximum {maxval} and minimum {minval} elements of the array: {dif}");
