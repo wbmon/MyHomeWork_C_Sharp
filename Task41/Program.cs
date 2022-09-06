69278a77cfdcea5dabe977d930796d87967dc8e4
Dmitry Molchanov <admin@nevinka-school1.ru>
2022-09-06 08:28:48 +0300

    Added the solution to task #41

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int FillArrayAndSearchPositive(int[] array)
+{
+    int positive = 0;
+    for (int i=0; i < array.Length; i++) 
+    {
+        Console.Write($"Enter {i+1} number: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+        if (array[i] > 0) positive++;
+    }
+return positive;
+}
+
+void PrintResult(int[] array, int positive)
+{
+    Console.Write($"You have been given the following numbers: ");
+    for (int i=0; i < array.Length; i++)
+    {
+    if (i <  array.Length - 1) Console.Write($"{array[i]},  ");
+    else Console.WriteLine($"{array[i]}");
+    }
+    Console.WriteLine($"There are {positive} positive numbers among them.");
+}
+
+Console.Clear();
+int m = 0;
+Console.Write("Set how many numbers we will enter: ");
+m = Convert.ToInt32(Console.ReadLine());
+
+int [] arr = new int[m];
+int pos = FillArrayAndSearchPositive(arr);
+PrintResult(arr, pos);
