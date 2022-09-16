f31b6821be41bdb42bbbdbb043c59820f43a30da
Dmitry Molchanov <admin@nevinka-school1.ru>
2022-09-16 21:26:22 +0300

    Added the solution to task #64

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+void PrintNumber(int n, int m)
+{
+    if (n != m) Console.Write(n + ", ");
+    else Console.Write(n);
+    if (n < m) PrintNumber(n + 1, m);
+}
+
+int InitialData(string inStr)
+{
+    Console.Write(inStr);
+    int outNum = Convert.ToInt32(Console.ReadLine());
+    return outNum;
+}
+
+Console.Clear();
+
+int n = InitialData("Set the value of the number N: ");
+int m = InitialData("Set the value of the number M: ");
+
+    if (m < n)
+    {
+        int temp = m;
+        m = n;
+        n = temp;
+    }
+
+PrintNumber(n, m);
